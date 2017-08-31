5720a5f12b24b327e925049ae7c54cbe26008eb2
micromanyth <miromanyth@gmail.com>
2017-08-31 17:56:38 -0400

    added grid generation and coloring functionality

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-function grid(numBox) {
-	$(document).ready(function() {	
-	for (var i = 0;i < numBox; i++) {
-		$('#sketchpad').append("<div></div>");
-		console.log(numBox);
-	}
-	});
+
+function createGrid(x) {
+  for (var rows = 0; rows < x; rows++){
+    for (var cols = 0; cols < x; cols++){
+      $("#sketchpad").append("<div class = 'grid' ></div>");
+    };
+  };
+  $(".grid").height(600/x);
+  $(".grid").width(800/x);
 };
 
+//Clear grid function
+function clearGrid() {
+  $(".grid").remove();
+}
 
-grid(16);
+// Create default 16x16 grid, and set to color black 
+$(document).ready(function(){
+  createGrid(16);
+  $('.grid').hover(function() {
+  	$(this).css('background-color', 'black');
+  });
+});
+
+// If button "clear grid" is pressed
+$(document).ready(function(){
+	$(".clearGrid").click(function(){
+	  clearGrid();
+	  createGrid(16);
+	  $(".grid").hover(function(){
+	    $(this).css("background-color", "black");
+	   });
+	});
+});
+
+
+
+
+
